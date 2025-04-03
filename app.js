b2a3d18e4deaf9b2d3e755f0b32ebc1bd184ae0e
yasin-frontend <yasin2009revenge@gmail.com>
2025-04-03 10:50:27 +0500

    third commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 const users = [
     { user_id: 1, name: 'Yasin', favorite_product: 'Duo Daggers', age: 16 },
@@ -10,15 +11,32 @@ const users = [
     { user_id: 6, name: 'Nolan', favorite_product: 'Knife', age: 14 },
 ]
 
+const API_KEY = process.env.API_KEY;
+
+const authApiKey = async (req, res, next) => {
+    const apiKey = req.headers['x-api-key'];
+
+    if (!apiKey) {
+        return res.status(401).json({ error: 'Api key is missing' });
+    }
+
+    if (apiKey !== API_KEY) {
+        return res.status(403).json({ error: 'Invalid Api key' });
+    }
+
+    next();
+}
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-api-key');  // Добавляем x-api-key сюда
     res.setHeader('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') return res.status(200);
     next();
 });
 
-app.get('/api', (req, res) => {
+app.get('/api', authApiKey, (req, res) => {
     res.json(users);
 });
 
